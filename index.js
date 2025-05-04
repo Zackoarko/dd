971dee415c22b7cd0c2ecce291ab9dc91a3aebf3
Zackoarko <margotribere6@gmail.com>
2025-05-04 15:44:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 ff", "🎮 ff"];
+const statusMessages = ["🎧 ff", "Je vénère seulement zack"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
